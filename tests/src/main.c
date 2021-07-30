06f5d680a238e7241169e57ab875af4ed2cfdd94
Shawn M. Chapla <shawn@chapla.email>
2021-07-29 23:17:56 -0400

    Begin migrating debug repl code into tests

--- tests/src/main.c
+++ tests/src/main.c
@@ -20,6 +20,9 @@ struct e4__task* e4t__transient_task(void)
 
 int main(void)
 {
+    /* FIXME: Move this code somewhere better. */
+    struct e4__task *task;
+
     /* Test asserts themselves. Sanity check. */
     e4t__ASSERT_OK(e4__E_OK);
     e4t__ASSERT_EQ(923816, 923816);
@@ -31,6 +34,38 @@ int main(void)
     e4t__term_selftest();
 
     /* Call into actual tests functions. */
+    /* FIXME: Move these tests somewhere appropriate. */
+    task = e4t__transient_task();
+    e4t__ASSERT_OK(e4__evaluate(task, "1 -2 3 .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<3> 1 -2 3");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 clear .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<0>");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 drop .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<2> 1 2");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 dup .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<4> 1 2 3 3");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 over .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<4> 1 2 3 2");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 rot .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<3> 2 3 1");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 swap .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<3> 1 3 2");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 tuck .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<4> 1 3 2 3");
+
+    e4t__ASSERT_OK(e4__evaluate(task, "1 2 3 4 5 4 roll .s clear", -1, 0));
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "<5> 1 3 4 5 2");
+
+    e4t__ASSERT_EQ(e4__evaluate(task, "1 2 3 4 5 4 rll .s clear", -1, 0),
+            e4__E_UNDEFWORD);
+    e4t__ASSERT_MATCH(e4t__term_obuf_consume(), "");
 
     /* Summarize test results. */
     fprintf(stderr, "Tests complete! (%lu of %lu passed; %lu failed)\n",
